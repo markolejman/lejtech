4807161321b7105cdb1a01ffed93cde5fe1e7e68
markohautala <markohautala@live.se>
2025-09-30 13:05:08 +0200

    Update comments

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,6 +1,6 @@
 'use client'
 
-// Inspired by react-hot-toast library
+// This is inspired by react-hot-toast library
 import * as React from 'react'
 
 import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
